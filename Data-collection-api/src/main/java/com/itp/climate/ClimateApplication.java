4b2c9b1c4ea52d098fc4fd602cbd26d404bb748a
itp9177 <indutharanga@gmail.com>
2024-04-04 22:00:36 +0530

    springboot project for data collection

---
+++ Data-collection-api/src/main/java/com/itp/climate/ClimateApplication.java
@@ -0,0 +1,13 @@
+package com.itp.climate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimateApplication.class, args);
+	}
+
+}
---
+++ Data-collection-api/src/test/java/com/itp/climate/ClimateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itp.climate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
